b51467836ab1143a00de31ce2b9924b3c94e6643
Şemal Genç <139345662+semal-genc@users.noreply.github.com>
2025-09-27 21:58:56 +0300

    If-ElseIf-Ternary-If file added

---
+++ _posts/C#-101/If-ElseIf-Ternary-If/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Bir sayı girin: ");
+int sayi = Convert.ToInt32(Console.ReadLine());
+
+// If-Else ile kontrol
+if (sayi % 2 == 0)
+{
+    Console.WriteLine("Sayı çift");
+}
+else
+{
+    Console.WriteLine("Sayı tek");
+}
+
+// Ternary operatörü ile kontrol
+string sonuc = (sayi % 2 == 0) ? "Sayı çift" : "Sayı tek";
+Console.WriteLine("Ternary ile: " + sonuc);
+
+if (sayi > 0)
+{
+    Console.WriteLine("Sayı pozitif");
+}
+else if (sayi < 0)
+{
+    Console.WriteLine("Sayı negatif");
+}
+else
+{
+    Console.WriteLine("Sayı sıfır");
+}
