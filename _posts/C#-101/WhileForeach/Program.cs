865b14f01c7c11067a881f5c72814354992fe520
Şemal Genç <139345662+semal-genc@users.noreply.github.com>
2025-09-28 14:14:40 +0300

    WhileForeach file added

---
+++ _posts/C#-101/WhileForeach/Program.cs
@@ -0,0 +1,20 @@
+﻿int sayac = 1;
+int toplamWhile = 0;
+
+while (sayac <= 10)
+{
+    toplamWhile += sayac;
+    sayac++;
+}
+Console.WriteLine("1'den 10'a kadar sayıların toplamı (while): " + toplamWhile);
+
+int[] sayilar = { 5, 10, 15, 20, 25 };
+int toplamForeach = 0;
+
+foreach (int sayi in sayilar)
+{
+    toplamForeach += sayi;
+}
+
+double ortalama = (double)toplamForeach / sayilar.Length;
+Console.WriteLine("Dizideki sayıların ortalaması (foreach): " + ortalama);
