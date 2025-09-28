0446188bfcb0cd4de433ca7b7c9e82238df246d2
Şemal Genç <139345662+semal-genc@users.noreply.github.com>
2025-09-28 15:04:53 +0300

    MetotTanimlama file added

---
+++ _posts/C#-101/MetotTanimlama/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    public int ArttırVeTopla(ref int x, ref int y)
+    {
+        x += 1;
+        y += y;
+        return x + y;
+    }
+
+    private static void Main(string[] args)
+    {
+        Program instance = new Program();
+
+        int a = 3;
+        int b = 4;
+
+        Console.WriteLine("Metot çağrılmadan önce:");
+        Console.WriteLine("a = " + a);
+        Console.WriteLine("b = " + b);
+
+        int sonuc = instance.ArttırVeTopla(ref a, ref b);
+
+        Console.WriteLine("\nMetot çağrıldıktan sonra:");
+        Console.WriteLine("a = " + a);
+        Console.WriteLine("b = " + b);
+        Console.WriteLine("Sonuç = " + sonuc);
+    }
+}
