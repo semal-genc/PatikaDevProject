efdf5bf7c2512d1cf838ec62e41bcbe9390a5091
Şemal Genç <139345662+semal-genc@users.noreply.github.com>
2025-09-28 15:11:56 +0300

    MetotOverloading file added

---
+++ _posts/C#-101/MetotOverloading/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    public void EkranaYazdir(int deger)
+    {
+        Console.WriteLine("Int: " + deger);
+    }
+
+    public void EkranaYazdir(string deger)
+    {
+        Console.WriteLine("String: " + deger);
+    }
+
+    public void EkranaYazdir(string deger1, string deger2)
+    {
+        Console.WriteLine("String + String: " + deger1 + deger2);
+    }
+
+    public int Toplam(int a, int b, out int toplam)
+    {
+        toplam = a + b;
+        return toplam;
+    }
+
+    private static void Main(string[] args)
+    {
+        Program instance = new Program();
+
+        instance.EkranaYazdir(5);
+        instance.EkranaYazdir("Merhaba");
+        instance.EkranaYazdir("Merhaba", " Dünya");
+
+        Console.WriteLine();
+
+        int sonuc;
+        int toplamSonuc = instance.Toplam(4, 8, out sonuc);
+
+        Console.WriteLine("Toplam (out ile): " + sonuc);
+        Console.WriteLine("Toplam (return ile): " + toplamSonuc);
+    }
+}
