a85ef2cb282618a9ea3f06778cb3d1d049b38000
Şemal Genç <139345662+semal-genc@users.noreply.github.com>
2025-10-22 13:29:09 +0300

    KurucuMetotlar file added

---
+++ _posts/C#-101/KurucuMetotlar/Program.cs
@@ -0,0 +1,50 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("***********Öğrenci 1***********");
+        Ogrenci ogrenci1 = new Ogrenci("sahih", "özden", 214);
+        ogrenci1.OgrenciBilgileri();
+
+        Console.WriteLine("***********Öğrenci 2***********");
+        Ogrenci ogrenci2 = new Ogrenci();
+        ogrenci2.ad = "saliha";
+        ogrenci2.soyad = "gökçen";
+        ogrenci2.ogrNo = 664;
+        ogrenci2.OgrenciBilgileri();
+
+        Console.WriteLine("***********Öğrenci 3***********");
+        Ogrenci ogrenci3 = new Ogrenci("rüştü", "sönmez");
+        ogrenci3.OgrenciBilgileri();
+    }
+}
+
+class Ogrenci
+{
+    public string? ad; //başlangıçta null olabilir anlamını taşır.
+    public string? soyad; //başlangıçta null olabilir anlamını taşır.
+    public int ogrNo;
+
+    public Ogrenci(string ad, string soyad, int ogrNo)
+    {
+        this.ad = ad;
+        this.soyad = soyad;
+        this.ogrNo = ogrNo;
+    }
+
+    public Ogrenci() { }
+
+
+    public Ogrenci(string ad, string soyad)
+    {
+        this.ad = ad;
+        this.soyad = soyad;
+    }
+
+    public void OgrenciBilgileri()
+    {
+        Console.WriteLine($"Öğrenci Adı: {ad}");
+        Console.WriteLine($"Öğrenci Soyadı: {soyad}");
+        Console.WriteLine($"Öğrenci Numarası: {ogrNo}");
+    }
+}
