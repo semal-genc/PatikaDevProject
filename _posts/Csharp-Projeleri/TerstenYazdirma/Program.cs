8a1df7673f81eb3f6480439c8ebb67b4b9aabb27
Şemal Genç <139345662+semal-genc@users.noreply.github.com>
2025-11-18 16:00:20 +0300

    TerstenYazdirma file added

---
+++ _posts/Csharp-Projeleri/TerstenYazdirma/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ReverseProcess reverseProcess = new ReverseProcess();
+        reverseProcess.ReverseString();
+    }
+}
+
+public class InputService
+{
+    public string GetInput()
+    {
+        Console.Write("Lütfen bir metin giriniz: ");
+        return Console.ReadLine() ?? "";
+    }
+}
+
+public class ReverseProcess
+{
+    public readonly InputService input = new InputService();
+    public void ReverseString()
+    {
+        string word = input.GetInput();
+        string[] words = word.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            words[i]=new string(words[i].Reverse().ToArray());
+        }
+
+        Console.WriteLine(string.Join(' ', words));
+    }
+}
