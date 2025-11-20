eda308ff531f3d87ec2e22a09298c826b8b90e51
Şemal Genç <139345662+semal-genc@users.noreply.github.com>
2025-11-20 10:26:39 +0300

    KarakterDegistirme file added

---
+++ _posts/Csharp-Projeleri/KarakterDegistirme/Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        SentenceProcess sentenceProcess=new();
+        string[] sentence=sentenceProcess.ChangeSentence();
+        Console.WriteLine(string.Join(' ', sentence));
+    }
+}
+
+public class InputService
+{
+    public string ReadString()
+    {
+        Console.Write("Bir metin giriniz: ");
+        return Console.ReadLine() ?? "";
+    }
+}
+
+public class SentenceProcess
+{
+    InputService inputService = new();
+
+    public static string SwapFirstLast(string word)
+    {
+        if (word.Length < 2)
+            return word;
+        return word[^1] + word.Substring(1, word.Length - 2) + word[0];
+    }
+
+    public string[] ChangeSentence()
+    {
+        string sentence = inputService.ReadString();
+
+        string[] words = sentence.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            words[i] = SwapFirstLast(words[i]);
+        }
+        return words;
+    }
+}
