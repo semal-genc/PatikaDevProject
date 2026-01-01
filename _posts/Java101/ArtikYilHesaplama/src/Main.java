d6644d72ea7a6770283a910c89f92b3e84064e95
Şemal Genç <139345662+semal-genc@users.noreply.github.com>
2026-01-01 21:58:21 +0300

    ArtikYilHesaplama added

---
+++ _posts/Java101/ArtikYilHesaplama/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int yil;
+
+        System.out.print("Yıl Giriniz : ");
+        yil = input.nextInt();
+
+        if (yil % 400 == 0) {
+            System.out.println(yil + " bir artık yıldır !");
+        } 
+        else if (yil % 100 == 0) {
+            System.out.println(yil + " bir artık yıl değildir !");
+        } 
+        else if (yil % 4 == 0) {
+            System.out.println(yil + " bir artık yıldır !");
+        } 
+        else {
+            System.out.println(yil + " bir artık yıl değildir !");
+        }
+    }
+}
